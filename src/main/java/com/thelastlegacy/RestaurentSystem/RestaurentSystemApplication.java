5e5bb43a694b50ef61e76582e3148513c165109b
sanrumbas <sanrumbas@gmail.com>
2017-09-07 10:16:48 +0545

    first commit

---
+++ src/main/java/com/thelastlegacy/RestaurentSystem/RestaurentSystemApplication.java
@@ -0,0 +1,12 @@
+package com.thelastlegacy.RestaurentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurentSystemApplication.class, args);
+	}
+}
